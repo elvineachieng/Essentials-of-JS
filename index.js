4284c79d1d882afdb86b79f039eb5e6d52a3c8d2
Elvine7067 <achiengelvine1@gmail.com>
2023-07-03 14:58:51 +0300

    essentials of js

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log(10+10);
+let arr3 = new Array(10);
+let arr4 = [10];
+console.log(arr3);
+console.log(arr4);
+let cars=["Toyota","Isuzu","Volkswagen"];
+console.log(cars[0]);
+cars[0]="Tesla"
+console.log(cars);
+let shoppingList=["Milk","Bread","Apples"];
+console.log(shoppingList);
+shoppingList[1]="Bananas";
+console.log(shoppingList);
