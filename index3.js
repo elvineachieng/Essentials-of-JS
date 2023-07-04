278f005ff6409f0c841d1d8704582f04846da327
Elvine7067 <achiengelvine1@gmail.com>
2023-07-04 15:05:54 +0300

    shift() method

--- index3.js
+++ index3.js
@@ -3,3 +3,9 @@
 let arr8=[1, 2, 3, 4, 5, 6, 7, 8, 9];
 arr8.pop();
 console.log(arr8);
+//shift() is used to delete the first element. It causes all other indices to be reduced by one
+arr8.shift();
+console.log(arr8);
+//splice can also be used to delet elements
+arr8.splice(1,3);
+console.log(arr8);
