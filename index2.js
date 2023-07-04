59b75a9562feeb7a29ec2b6ab3c2c1001c3c9d6a
Elvine7067 <achiengelvine1@gmail.com>
2023-07-04 14:33:07 +0300

    splice method

--- index2.js
+++ index2.js
@@ -1,4 +1,9 @@
 favoriteFruits = ["grapefruit", "orange", "lemon"];
 favoriteFruits.push("tangerine");
 console.log(favoriteFruits);
-
+//splice() method for adding an element at a certain index
+let arrOfShapes = ["circle","triangle","rectangle","pentagon"];
+arrOfShapes.splice(2,0, "square","trapezoid");
+console.log(arrOfShapes);
+arrOfShapes.splice(2,2, "square","trapezoid")//we start inserting from index 2 and second parameter 2 is the  umber of elements we delete  starting from our previously define starting index
+console.log(arrOfShapes);
