407871e5c7f4b022f5f0fa5adebb96a9438ff437
Elvine7067 <achiengelvine1@gmail.com>
2023-07-04 11:48:26 +0300

    built in length property

--- index1.js
+++ index1.js
@@ -2,5 +2,14 @@ let cars=["Toyota","Renault","Volswagen"];
 let myArray= cars;
 console.log(myArray)
 cars[0]="Tesla";
-console.log(myArray);
+console.log(cars);
 console.log(myArray[0]);
+//lets try to overwrite elements that dont exist
+console.log(cars[3]);
+//built in length property in arrays
+colors = ["black", "orange", "pink"]
+booleans = [true, false, false, true];
+emptyArray = [];
+console.log("Length of colors:", colors.length);
+console.log("Length of booleans:", booleans.length);
+console.log("Length of empty array:", emptyArray.length);
