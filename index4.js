be6994ef9b22019c602efeec450fdfd466a2aefa
Elvine7067 <achiengelvine1@gmail.com>
2023-07-05 09:44:55 +0300

    IndexOf() method

--- index4.js
+++ index4.js
@@ -3,4 +3,9 @@ arr8=[2,6,7,8];
 let findValue=arr8.find(function (e){return e == 6});
 let findValue2=arr8.find (e=> e===10);
 console.log(findValue, findValue2);
-//will log 6 and undefined since arr8 doesnt have 10g
+//will log 6 and undefined since arr8 doesnt have 10
+//knowing what position an element is in an array indexOf methpd
+arr8 = [2, 6, 7, 8];
+let findIndex = arr8.indexOf(6);
+let findIndex2 = arr8.indexOf(10);
+console.log(findIndex, findIndex2);
