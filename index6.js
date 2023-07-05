95ac29cbc0e1f87ef4d8776cfee34328aa3bb7e5
Elvine7067 <achiengelvine1@gmail.com>
2023-07-05 14:20:28 +0300

    multidimensiona arrays

---
+++ index6.js
@@ -0,0 +1,7 @@
+//Multidimensional arrays
+let someValues1 = [1, 2, 3];
+let someValues2 = [4, 5, 6];
+let someValues3 = [7, 8, 9];
+let arrOfArrays = (someValues1, someValues2, someValues3);
+console.log(arrOfArrays);
+
