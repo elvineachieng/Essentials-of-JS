522b5410f56f8aa31f0b766d519c6bec119c7aed
Elvine7067 <achiengelvine1@gmail.com>
2023-07-06 12:30:39 +0300

    objects in arrays

--- index7.js
+++ index7.js
@@ -67,4 +67,20 @@ let company = { companyName: "Healthy Candy",
  };
  let activity = company.activities[1]
  console.log(activity);
- 
+ //objects in arrays
+ let addresses = [{
+    street: "2nd street",
+    number: "123",
+    zipcode: "33116",
+    city: "Miami",
+    state: "Florida"
+    },
+    {
+    street: "1st West avenue",
+    number: "5",
+    zipcode: "75001",
+    city: "Addison",
+    state: "Texas"
+    }];
+    let streetName = addresses[0].street;
+    console.log(streetName);
