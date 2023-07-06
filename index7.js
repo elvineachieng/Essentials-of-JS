dfc7700701ea12a9d45557bc25dd9d4a4466f462
Elvine7067 <achiengelvine1@gmail.com>
2023-07-06 09:24:38 +0300

    accessing properties of an object

--- index7.js
+++ index7.js
@@ -1,3 +1,15 @@
 //objects in javascript
 let arr = [0, 1, 2];
 console.log(typeof arr);
+//output is object
+//objects are used to group multiple objects
+let dog = { dogName: "Swanky", 
+ weight: 2.4, 
+ color: "brown", 
+ breed: "chihuahua", 
+ age: 3, 
+ burglarBiter: true
+ };
+ console.log(dog);
+ let dogColor1 = dog ["color"];
+ console.log(dogColor1)
