f1348594f39165c1e4aafd62e0c1cd6a784566c1
Elvine7067 <achiengelvine1@gmail.com>
2023-07-06 09:04:53 +0300

    multidimensional arrays

--- index6.js
+++ index6.js
@@ -8,3 +8,7 @@ let arrArrays = [[1, 2, 3],[4, 5, 6],[7, 8, 9]];
 ///accesing elements of an inner array
 let value1 = arrArrays[0][1];//the statement grabs the first array and from this array it picks the second value
 console.log(value1);
+let value2 = arrOfArrays[2][2];
+console.log(value2);
+let arrOfArraysOfArrays = [arrOfArrays, arrOfArrays, arrOfArrays];
+console.log(arrOfArraysOfArrays);
