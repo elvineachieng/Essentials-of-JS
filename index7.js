80fec1d7d3f24a9a72192488a0c86e9e1c79e8cf
Elvine7067 <achiengelvine1@gmail.com>
2023-07-06 12:41:49 +0300

    objets in arrays in objects

--- index7.js
+++ index7.js
@@ -82,5 +82,26 @@ let company = { companyName: "Healthy Candy",
     city: "Addison",
     state: "Texas"
     }];
-    let streetName = addresses[0].street;
-    console.log(streetName);
+let streetName = addresses[0].street;
+console.log(streetName);
+    //objects in arrays in objects
+company = { companyName: "Healthy Candy",
+          activities: [ "food manufacturing","improving kids' health","manufacturing toys"],
+ address: [{
+ street: "2nd street",
+ number: "123",
+ zipcode: "33116",
+city: "Miami",
+ state: "Florida"
+ },
+ {
+ street: "1st West avenue",
+ number: "5",
+ zipcode: "75001",
+city: "Addison",
+state: "Texas"
+ }],
+ yearOfEstablishment: 2021
+ };
+let streetName1 = company.address[0].street;
+console.log(streetName1);
