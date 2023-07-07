e991f9bc425d8f3ea14b464a62e89646d4f3e82e
Elvine7067 <achiengelvine1@gmail.com>
2023-07-07 10:31:09 +0300

    project

---
+++ index8.js
@@ -0,0 +1,23 @@
+//Company project catalog
+let items=[];
+let item1={
+    name: "book",
+    model: "model1",
+    cost: 123,
+    quantity:500
+}
+let item2={
+    name: "pencils",
+    model: "model2",
+    cost: 124,
+    quantity:502
+}
+let item3={
+    name: "pens",
+    model: "model3",
+    cost: 125,
+    quantity:503
+}
+item.push(item1, item2, item3);
+console.log(inventory);
+
